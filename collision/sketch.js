298b315d4299543c2bf61744e66c2aee3ecf96eb
GeorgeWBush758 <161519487+GeorgeWBush758@users.noreply.github.com>
2024-03-08 11:15:05 -0600

    trjdfgfhsgurs

--- collision/sketch.js
+++ collision/sketch.js
@@ -10,6 +10,32 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+let hit = false;
+
 function draw() {
-  background(220);
+  background(255);
+  circle(200, 200, 100);
+  circle(mouseX, mouseY, 150);
+
+  hit = collideCircleCircle(mouseX, mouseY, 150, 200, 200, 100);
+
+  // Use vectors as input:
+  // const mouse  = createVector(mouseX, mouseY);
+  // const circle = createVector(200, 200);
+  // hit = collideCircleCircleVector(mouse, 150, circle, 100);
+
+  if (hit) {
+    stroke("red");
+  }
+  else {
+    stroke("black");
+  }
+
+
+  print("colliding?", hit);
 }
+
+
+
+
+
