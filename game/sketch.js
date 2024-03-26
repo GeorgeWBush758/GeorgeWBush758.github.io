8d6bbd9a6012624b68b7c5eb9b0a472c637c9943
GeorgeWBush758 <161519487+GeorgeWBush758@users.noreply.github.com>
2024-03-26 14:11:14 -0600

    Update sketch.js
    cookiec

--- game/sketch.js
+++ game/sketch.js
@@ -1,15 +1,23 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// make a game using an array 
+//make a cookie clicker game where balss from the top and dissapear at the bottom
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
+  drawCircle();
+  circle();
+
+}
+
+
+
+function drawCircle() {
+  fill(181, 101, 29);
+  circle(width/2, height/2, 50);
+  circle(width/2, height/2, 20);
 }
